1848941c725f89894b41826b7bb0fb0239ad4a88
Prerith Subramanya <prerithsubramanya@gmail.com>
2023-04-28 10:56:45 +0530

    changed the main file to use gsheetdb

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import gspread
 import streamlit as st
 from gs_utils import (
     returnGSheet,
@@ -14,15 +13,17 @@ def main():
     submit_button = st.button("Submit")
 
     if submit_button and sheet_name:
-        gsheet = returnGSheet()
+        conn = returnGSheet()
         try:
-            sheet = gsheet.worksheet(sheet_name)
-        except gspread.WorksheetNotFound:
+            sheet = conn.get_worksheet(sheet_name)
+        except Exception:
             st.error(f"Worksheet '{sheet_name}' not found.")
             return
         repo = return_repo()
         try:
-            bulkCreateAndChangeURl(sheet.get_all_records(), sheet, repo, command)
+            rows = conn.select(f'SELECT * FROM "{sheet_name}"')
+            records = [dict(row) for row in rows]
+            bulkCreateAndChangeURl(records, sheet_name, repo, command)
             st.success("Operation completed successfully!")
         except Exception as e:
             st.error(f"failed to {command} the sheet, got {e}")
